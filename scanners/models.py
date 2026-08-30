a1509d2456bea770fc95f7f2f492b119c6b354b1
madiyarmoldakhmet <madiyarmoldakhmet@gmail.com>
2026-08-30 21:39:24 +0500

    feat: atomic dev update 21:39:24

--- scanners/models.py
+++ scanners/models.py
@@ -22,6 +22,7 @@ class ScannerType(str, Enum):
     PIP_AUDIT = "pip-audit"
     DAST = "dast"
     SCA = "sca"
+    SECRET = "secret"
     STRIX = "strix"
     MOBILE = "mobile"
     CUSTOM = "custom"
